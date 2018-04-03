9a57361c8986c307fc72290ed25782e9f29de86d
prepin <repinpv@gmail.com>
2018-04-03 14:26:03 +0700

    fix project

--- src/main/java/com/demo/gametask/GameTaskApplication.java
+++ src/main/java/com/demo/gametask/GameTaskApplication.java
@@ -1,4 +1,4 @@
-package com.task.gametask;
+package com.demo.gametask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/demo/gametask/GameTaskApplicationTests.java
+++ src/test/java/com/demo/gametask/GameTaskApplicationTests.java
@@ -1,4 +1,4 @@
-package com.task.gametask;
+package com.demo.gametask;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,5 +12,4 @@ public class GameTaskApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
 }
