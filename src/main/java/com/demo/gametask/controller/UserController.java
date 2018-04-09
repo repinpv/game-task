f92732b8f2d7ae6b30c68a327c4232784f5bd6a0
prepin <repinpv@gmail.com>
2018-04-09 18:38:13 +0700

    fix registration form validation errors

--- src/main/java/com/demo/gametask/controller/UserController.java
+++ src/main/java/com/demo/gametask/controller/UserController.java
@@ -6,13 +6,17 @@ import com.demo.gametask.service.UserService;
 import com.demo.gametask.validator.UserValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.*;
 
 @Controller
 public class UserController {
@@ -25,10 +29,13 @@ public class UserController {
 
     private final UserValidator userValidator;
 
-    public UserController(UserService userService, SecurityService securityService, UserValidator userValidator) {
+    private final MessageSource messageSource;
+
+    public UserController(UserService userService, SecurityService securityService, UserValidator userValidator, MessageSource messageSource) {
         this.userService = userService;
         this.securityService = securityService;
         this.userValidator = userValidator;
+        this.messageSource = messageSource;
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
@@ -43,10 +50,23 @@ public class UserController {
         userValidator.validate(userForm, bindingResult);
 
         if (bindingResult.hasErrors()) {
-            for (ObjectError error : bindingResult.getAllErrors()) {
-                final String code = error.getCode();
-                logger.debug(code);
+            final Map<String, List<String>> errorMap = new HashMap<>();
+            for (final ObjectError error : bindingResult.getAllErrors()) {
+                if (error instanceof FieldError) {
+                    FieldError fieldError = (FieldError) error;
+
+                    final String key = "error-" + fieldError.getField();
+                    List<String> errorList = errorMap.get(key);
+                    if (errorList == null) {
+                        errorList = new ArrayList<>();
+                        errorMap.put(key, errorList);
+                    }
+
+                    final String message = messageSource.getMessage(error, Locale.ENGLISH);
+                    errorList.add(message);
+                }
             }
+            model.addAllAttributes(errorMap);
 
             return "registration";
         }
